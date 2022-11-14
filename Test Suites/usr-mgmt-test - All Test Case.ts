485483f6d8ea6efcbdb2d76e6d435db0fa881dd9
yunidharmadi <45481383+yunidharmadi@users.noreply.github.com>
2022-11-14 14:55:11 +0700

    first commit

---
+++ Test Suites/usr-mgmt-test - All Test Case.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>usr-mgmt-test - All Test Case</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0071e7b7-56ae-4d0d-a613-dce621fa8e63</testSuiteGuid>
+</TestSuiteEntity>
